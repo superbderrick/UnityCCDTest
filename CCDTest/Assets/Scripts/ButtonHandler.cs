f9f3a0e1b0942b313c3bd71c6e65e363d51f4034
superbderrick <kang.derrick@gmail.com>
2020-02-19 18:00:17 +0900

    added a sample project

---
+++ CCDTest/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public GameObject _Sphere;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveSphere()
+    {
+        Debug.Log("MoveSphere");
+    }
+    
+    public void StopSphere()
+    {
+        Debug.Log("StopSphere");
+    }
+    
+    public void ResetSphere()
+    {
+        Debug.Log("ResetSphere");
+    }
+    
+}
