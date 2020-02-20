acf342e01a57ac29fa9798b52a49fb3a64f06f4f
superbderrick <kang.derrick@gmail.com>
2020-02-20 16:45:21 +0900

    updated more

--- CCDTest/Assets/Scripts/ButtonHandler.cs
+++ CCDTest/Assets/Scripts/ButtonHandler.cs
@@ -5,32 +5,41 @@ using UnityEngine;
 public class ButtonHandler : MonoBehaviour
 {
     public GameObject _Sphere;
-    
+
+    private Rigidbody _Rigidbody;
+
+    private float SPEED = 100.0f;
+    private float X_A = 50.0f;
     
     void Start()
     {
-        
+        _Rigidbody = _Sphere.GetComponent<Rigidbody>();
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void MoveSphere()
     {
-        Debug.Log("MoveSphere");
+        
+        _Rigidbody.isKinematic = false;
+        _Rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        
+        Vector3 movemnet = new Vector3(X_A, 0, 0);
+        _Rigidbody.AddForce(movemnet * SPEED);
     }
     
     public void StopSphere()
     {
-        Debug.Log("StopSphere");
+        _Rigidbody.velocity = Vector3.zero;
+        _Sphere.transform.position = new Vector3(-10,0.5f,0); 
+            
+        
+        _Rigidbody.isKinematic = true;
+        _Rigidbody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+        
+     
     }
     
-    public void ResetSphere()
-    {
-        Debug.Log("ResetSphere");
-    }
+    
     
 }
